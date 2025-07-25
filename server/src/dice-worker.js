64c0b10fadd86f7abf225c2a63c8912901ee040b
John Paul Alabe <alabejohnpaul@gmail.com>
2025-07-25 22:32:12 +0800

    make ts

--- server/src/dice-worker.js
+++
@@ -1,7 +0,0 @@
-const { parentPort } = require('worker_threads');
-
-parentPort.on('message', () => {
-  // Simulate dice roll (could be replaced with heavy computation)
-  const roll = Math.floor(Math.random() * 6) + 1;
-  parentPort.postMessage(roll);
-});
